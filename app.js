9a5cc6ad4c4422c4ce949b3730f774e4925e8463
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-26 17:40:32 +0200

    mailchimp added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -16,13 +17,40 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-    const firstName=req.body.fname;
-    const lastName=req.body.lname;
-    const email=req.body.email;
-
-    console.log(firstName,lastName,email);
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME:firstName,
+        LNAME: lastName
+      }
+    }]
+  }
+  //console.log(firstName,lastName,email);
+  const jsonData = JSON.stringify(data);
+  const url="https://us14.api.mailchimp.com/3.0/lists/736289169b";
+const options={
+  method: "POST",
+  auth:"belma:1cacccd30c9a83428f428f2f451e13d6-us14"
+}
+  const request=https.request(url, options, function(response) {
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
 })
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
+
+//API
+//1cacccd30c9a83428f428f2f451e13d6-us14
+//listid
+//736289169b
